4efbd606cc3dc50ba0b5c975e93d8afea433a8df
YrYs <yryskeldi445@gmail.com>
2024-06-27 17:40:03 +0600

    bad pages animation

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,48 @@
-import { styled } from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { styled } from "@mui/material";
+import { Outlet, useLocation } from "react-router-dom";
+
+import { NavigateState } from "../utils/types";
+
 import SideBar from "../components/admin/SideBar";
+import "../index.css";
 
 interface LayoutProps {
-  navList: any;
+  navList: NavigateState[];
 }
 
 const Layout: React.FC<LayoutProps> = ({ navList }) => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = React.useState(location);
+  const [transitionStage, setTransitionStage] = React.useState("fadeIn");
+
+  React.useEffect(() => {
+    if (location !== displayLocation) setTransitionStage("fadeOut");
+  }, [location]);
+
+  React.useEffect(() => {
+    if (transitionStage === "fadeOut") {
+      const timeout = setTimeout(() => {
+        setTransitionStage("fadeIn");
+        setDisplayLocation(location);
+      }, 500); // Длительность анимации совпадает с CSS
+      return () => clearTimeout(timeout);
+    }
+  }, [transitionStage, location]);
+
   return (
     <LayoutStyle>
       <SideBar nav={navList} />
 
-      <Container>
+      <Container
+        transitionStage={transitionStage}
+        onAnimationEnd={() => {
+          if (transitionStage === "fadeOut") {
+            setTransitionStage("fadeIn");
+          }
+        }}
+        key={displayLocation.key}
+      >
         <Outlet />
       </Container>
     </LayoutStyle>
@@ -26,10 +56,21 @@ const LayoutStyle = styled("div")({
   gap: "30px",
 });
 
-const Container = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  gap: "30px",
-  marginRight: "30px",
-  width: "100%",
-});
+const Container = styled("div")<{ transitionStage: string }>(
+  ({ transitionStage }) => ({
+    display: "flex",
+    flexDirection: "column",
+    gap: "30px",
+    marginRight: "30px",
+    width: "100%",
+    animation: `${transitionStage} 0.5s cubic-bezier(0.25, 0.1, 0.25, 1) forwards`,
+    "@keyframes fadeIn": {
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+    },
+    "@keyframes fadeOut": {
+      from: { opacity: 1 },
+      to: { opacity: 0 },
+    },
+  })
+);
